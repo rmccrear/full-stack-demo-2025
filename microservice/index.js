85d0390e68a02fef2488187c8c2dd667b395d309
Robert McCreary Mannino <fooyinfighter@gmail.com>
2026-01-16 19:52:47 -0800

    format

--- microservice/index.js
+++ microservice/index.js
@@ -9,47 +9,44 @@ const weather_api_key = process.env.WEATHER_API_KEY;
 const weatherCache = {
     "LaLaLand": {
         days: [{ datetime: "2026-01-20", temp: 99, conditions: "Freezing" }]
-    }, 
+    },
     "Land of Oz": {
         days: [{ datetime: "2026-01-20", temp: 99, conditions: "Raining" }]
-    }, 
-    "Chicago": {
-        days: [{ datetime: "2026-01-20", temp: 99, conditions: "Raining" }]
-    }, 
+    },
 }
 
 
 
-    app.get("/weather-for-date/:date/:location", async (req, res) => {
-        const date = req.params.date;
-        const location = req.params.location;
-        console.log(weatherCache);
-        let data = null;
-        if (weatherCache[location]) {
-            console.log("Cache hit on ", location)
-            data = weatherCache[location];
-        } else if (!weatherCache[location]) {
-            console.log("Cache miss on ", location)
-            const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${weather_api_key}&contentType=json&include=days`;
-            const response = await fetch(url); // expensive
-            data = await response.json();
-            weatherCache[location] = data;
-        }
+app.get("/weather-for-date/:date/:location", async (req, res) => {
+    const date = req.params.date;
+    const location = req.params.location;
+    console.log(weatherCache);
+    let data = null;
+    if (weatherCache[location]) {
+        console.log("Cache hit on ", location)
+        data = weatherCache[location];
+    } else if (!weatherCache[location]) {
+        console.log("Cache miss on ", location)
+        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${weather_api_key}&contentType=json&include=days`;
+        const response = await fetch(url); // expensive
+        data = await response.json();
+        weatherCache[location] = data;
+    }
 
-        // just get the one day...
-        const weatherForDay = data.days.find((day) => day.datetime === date);
+    // just get the one day...
+    const weatherForDay = data.days.find((day) => day.datetime === date);
 
-        const weatherData = {
-            temp: weatherForDay.temp,
-            conditions: weatherForDay.conditions,
+    const weatherData = {
+        temp: weatherForDay.temp,
+        conditions: weatherForDay.conditions,
 
-        }
+    }
 
-        return res.json(weatherData);
-    })
+    return res.json(weatherData);
+})
 
 
 
 app.listen(PORT, () => {
-        console.log("started on port " + PORT);
-    });
+    console.log("started on port " + PORT);
+});
